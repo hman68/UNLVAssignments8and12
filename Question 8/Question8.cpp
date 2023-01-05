e06aa39c0059ae9fac5a4b8df1e767c65599d17a
hman68 <henrylepp3@gmail.com>
2023-01-05 13:29:22 -0800

    More Work on the Figure Eight

--- Question 8/Question8.cpp
+++ Question 8/Question8.cpp
@@ -6,9 +6,12 @@ using namespace std;
 void makeSlash(ofstream& oFile, int size, char c);
 void makeEight(ofstream& oFile, int size, char c);
 void makeTriangle(ofstream& oFile, int size, char c);
+void printCharacters(char c, int num);
 
 int main(){
-
+    ofstream outFile;
+    outFile.open("test.txt");
+    makeEight(outFile, 12, 'A');
 }
 
 
@@ -18,11 +21,48 @@ void makeEight(ofstream& oFile, int size, char c){
         edgeWidth = 2;
     }
     int specialRowCount = edgeWidth/2;
-    int section1Rows = size-(specialRowCount * 3) / 2;
+    int middleWidth = (size - (2*edgeWidth));
+    int section1Rows = (size-(specialRowCount * 3)) / 2;
     int section2Rows = section1Rows;
+    int count = 1;
     if((specialRowCount * 3) + section1Rows + section2Rows != size){
         section2Rows++;
     }
 
-    cout << section2Rows;
+    cout << "\t";
+    for(int i = 1; i <= size; i++){
+        cout<<i%10;
+    }
+    cout << endl << count << "\t";
+    printCharacters(' ', edgeWidth);
+    printCharacters(c, middleWidth);
+    count++;
+    for(int i = 0; i < section1Rows; i++){
+        cout << endl << count << "\t";
+        printCharacters(c, edgeWidth);
+        printCharacters(' ', middleWidth);
+        printCharacters(c, edgeWidth);
+        count++;
+    }
+    cout << endl << count << "\t";
+    printCharacters(' ', edgeWidth);
+    printCharacters(c, middleWidth);
+    count++;
+    for(int i = 0; i < section2Rows; i++){
+        cout << endl << count << "\t";
+        printCharacters(c, edgeWidth);
+        printCharacters(' ', middleWidth);
+        printCharacters(c, edgeWidth);
+        count++;
+    }
+    cout << endl << count << "\t";
+    printCharacters(' ', edgeWidth);
+    printCharacters(c, middleWidth);
+    count++;
+}
+
+void printCharacters(char c, int num){
+    for(int i = 0; i < num; i++){
+        cout << c;
+    }
 }
