c906e7aa7a00404a59c33415d1b8b5c9e3c8ea59
hman68 <henrylepp3@gmail.com>
2023-01-05 09:32:56 -0800

    Created Function Prototypes

--- Question 8/Question8.cpp
+++ Question 8/Question8.cpp
@@ -1,6 +1,12 @@
 #include <fstream>
 #include <iostream>
 
+using namespace std;
+
+void makeSlash(ofstream& oFile, int size, char c);
+void makeEight(ofstream& oFile, int size, char c);
+void makeTriangle(ofstream& oFile, int size, char c);
+
 int main(){
-    
+
 }
