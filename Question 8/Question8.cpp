2ac9fee5dbbfc1d8f87879da13b41caa94331a0d
hman68 <henrylepp3@gmail.com>
2023-01-08 20:31:52 -0800

    Added Comments, Matched functions to those described in problem

--- Question 8/Question8.cpp
+++ Question 8/Question8.cpp
@@ -7,63 +7,75 @@ using namespace std;
 void makeSlash(ofstream& oFile, int size, char c);
 void makeEight(ofstream& oFile, int size, char c);
 void makeTriangle(ofstream& oFile, int size, char c);
-void printCharacters(char c, int num);
+void outputChar(ofstream& oFile,  int count, char outChar);
+
 
 int main(){
     ofstream outFile;
-    outFile.open("test.txt");
-    makeEight(outFile, 12, 'A');
+    makeEight(outFile, 31, 'A');
 }
 
 
 void makeEight(ofstream& oFile, int size, char c){
-    int edgeWidth = size/10;
-    if(edgeWidth < 2){
+    //All variables are assigned as detailed in the instructions
+    int edgeWidth = size/10; // The width of the "sides" of the Figure Eight
+    if(edgeWidth < 2){ 
         edgeWidth = 2;
     }
-    int specialRowCount = edgeWidth/2;
-    int middleWidth = (size - (2*edgeWidth));
-    int section1Rows = (size-(specialRowCount * 3)) / 2;
-    int section2Rows = section1Rows;
-    int count = 1;
-    if((specialRowCount * 3) + section1Rows + section2Rows != size){
+    int specialRowCount = edgeWidth/2; // Determine the width of the top, middle, and bottom rows
+    int middleWidth = (size - (2*edgeWidth));//The number of spaces between the two vertical "sides" of the figure
+    int section1Rows = (size-(specialRowCount * 3)) / 2;//The number of rows in the top of the figure eight
+    int section2Rows = section1Rows; //The number of rows in the bottom of the figure eight
+    int count = 1;//Current row being printed
+    if((specialRowCount * 3) + section1Rows + section2Rows != size){//If there are not enough rows, add one to the bottom section
         section2Rows++;
     }
 
+    cout << "Symbol: Figure Eight\tSize: "<<size<<"\tCharacter: "<<c<<endl;
+    //Prints Column Count
     cout << "\t";
     for(int i = 1; i <= size; i++){
         cout<<i%10;
     }
+    //Print the top row of the Figure Eight
     cout << endl << setw(2) << right <<  count << "\t";
-    printCharacters(' ', edgeWidth);
-    printCharacters(c, middleWidth);
+    outputChar(oFile, edgeWidth, ' ');
+    outputChar(oFile, middleWidth, c);
     count++;
+
+    //Print out the "vertical" rows of the top section of the Figure Eight
     for(int i = 0; i < section1Rows; i++){
         cout << endl << setw(2) << right << count << "\t";
-        printCharacters(c, edgeWidth);
-        printCharacters(' ', middleWidth);
-        printCharacters(c, edgeWidth);
+        outputChar( oFile, edgeWidth, c);
+        outputChar( oFile, middleWidth, ' ');
+        outputChar( oFile, edgeWidth, c);
         count++;
     }
+
+    //Print the middle row of the Figure Eight
     cout << endl << setw(2) << right << count << "\t";
-    printCharacters(' ', edgeWidth);
-    printCharacters(c, middleWidth);
+    outputChar( oFile, edgeWidth, ' ');
+    outputChar( oFile, middleWidth, c);
     count++;
+
+    //Print out the "vertical" rows of the bottom section of the Figure Eight
     for(int i = 0; i < section2Rows; i++){
         cout << endl << setw(2) << right << count << "\t";
-        printCharacters(c, edgeWidth);
-        printCharacters(' ', middleWidth);
-        printCharacters(c, edgeWidth);
+        outputChar( oFile, edgeWidth, c);
+        outputChar( oFile, middleWidth, ' ');
+        outputChar( oFile, edgeWidth, c);
         count++;
     }
+
+    //Print out the bottom row of the Figure Eight
     cout << endl << setw(2) << right << count << "\t";
-    printCharacters(' ', edgeWidth);
-    printCharacters(c, middleWidth);
+    outputChar( oFile, edgeWidth, ' ');
+    outputChar( oFile, middleWidth, c);
     count++;
 }
 
-void printCharacters(char c, int num){
-    for(int i = 0; i < num; i++){
-        cout << c;
+void outputChar(ofstream& oFile, int count, char outChar){
+    for(int i = 0; i < count; i++){
+        cout << outChar;
     }
 }
