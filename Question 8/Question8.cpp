dc94a235a77ddc9fde35fc1b45ec56c6fd9cc735
Philip Foels <philip.137862@nv.ccsd.net>
2023-01-09 09:46:17 -0800

    Adding to Commands

--- Question 8/Question8.cpp
+++ Question 8/Question8.cpp
@@ -6,12 +6,60 @@ using namespace std;
 void makeSlash(ofstream& oFile, int size, char c);
 void makeEight(ofstream& oFile, int size, char c);
 void makeTriangle(ofstream& oFile, int size, char c);
+bool readRecord (ifstream& iFile, string& typeString, int& lSize, char& c);
+void openInputFile(ifstream& iFile, string prompt);
+bool openOutputFile(ofstream& oFile, string fname);
+string createOutputFilename(string typeString, int labelSize);
 
 int main(){
-<<<<<<< HEAD
+    ifstream iFile;
+    ofstream oFile;
+    string type, title;
+    int size;
+    char c;
+    openInputFile(iFile, "test.txt");
+    while(readRecord(iFile, type, size, c)){
+        title = createOutputFilename( type, size);
+        openOutputFile(oFile, title);
+        oFile << type << title<<size<<c;
+        oFile.close();
+    }
+    iFile.close();
+    return 0;
+}
 
-=======
-   
-    
->>>>>>> 75cba86b410961b92ecc51482a8e39721bb8d06f
+bool readRecord (ifstream& iFile, string& typeString, int& lSize, char& c){
+    if(iFile.eof()){
+        return false;
+    }else{
+        if(iFile.peek() == '\n'){
+            iFile.ignore(100, '\n');
+        }
+        iFile >> typeString;
+        cout << typeString;
+        iFile >> lSize;
+        iFile >> c;
+        return true;
+    }
+}
+
+void openInputFile(ifstream& iFile, string prompt){
+    iFile.open(prompt);
+}
+
+bool openOutputFile(ofstream& oFile, string fname){
+    oFile.open(fname);
+    if (oFile.is_open()){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+string createOutputFilename(string typeString, int labelSize){
+    string filename;
+    string labelSizeString;
+    labelSizeString = to_string(labelSize);
+    filename = typeString + "_" + labelSizeString + ".txt";
+    return filename;
 }
