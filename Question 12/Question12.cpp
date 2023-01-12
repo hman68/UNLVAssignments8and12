ba6d2fb7bf4448fb09c36aeecc4d3a7326727863
Shane Wyman <88848611+Shane272361@users.noreply.github.com>
2023-01-12 09:14:29 -0800

    Update Question12.cpp

--- Question 12/Question12.cpp
+++ Question 12/Question12.cpp
@@ -1,6 +1,40 @@
-#include <fstream>
 #include <iostream>
-
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+void readFile( ifstream & inFile, vector<itemType> &item);
+bool openInputFile( ifstream & inFile );
+bool getQuotedString( string& line, int &index, string & subString);
+bool findIngredient( itemType item, string ingredient);
+string makeStringUpper( string s);
+int searchForIngredient( vector<itemType> & itemType, string ingredient, bool printEnable );
+void printRecord( itemType & item );
+void sort(vector<itemType>& myList);
 int main(){
+
+}
+void readFile( ifstream & inFile, vector<itemType> &item){
+
+}
+bool openInputFile( ifstream & inFile ){
+
+}
+bool getQuotedString( string& line, int &index, string & subString){
+
+}
+bool findIngredient( itemType item, string ingredient){
+
+}
+string makeStringUpper( string s){
+
+}
+int searchForIngredient( vector<itemType> & itemType, string ingredient, bool printEnable ){
+
+}
+void printRecord( itemType & item ){
+
+}
+void sort(vector<itemType>& myList){
     
 }
