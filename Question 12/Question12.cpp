3b584546b13be3c19628a77eabf416f7ec75b4d9
hman68 <henrylepp3@gmail.com>
2023-01-12 09:31:31 -0800

    Added itemType struct

--- Question 12/Question12.cpp
+++ Question 12/Question12.cpp
@@ -3,6 +3,18 @@
 #include <string>
 #include <vector>
 using namespace std;
+
+struct itemType {
+    unsigned int NBD_No;
+    string Long_Name;
+    string Data_Source;
+    unsigned long GTIN_UPC;
+    string Manufacturer;
+    string Date_Modified;
+    string Date_Available_Date;
+    string Ingredients;
+};
+
 void readFile( ifstream & inFile, vector<itemType> &item);
 bool openInputFile( ifstream & inFile );
 bool getQuotedString( string& line, int &index, string & subString);
@@ -11,6 +23,9 @@ string makeStringUpper( string s);
 int searchForIngredient( vector<itemType> & itemType, string ingredient, bool printEnable );
 void printRecord( itemType & item );
 void sort(vector<itemType>& myList);
+
+
+
 int main(){
 
 }
