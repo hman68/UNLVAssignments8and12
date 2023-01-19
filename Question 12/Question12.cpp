15e2c783979d0f44387875edcbe1d2d785ad20f1
hman68 <henrylepp3@gmail.com>
2023-01-19 09:49:03 -0800

    presentation

--- Question 12/Question12.cpp
+++ Question 12/Question12.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <iomanip>
 using namespace std;
 
 struct itemType {
@@ -9,7 +10,7 @@ struct itemType {
     unsigned int NBD_No;
     string Long_Name;
     string Data_Source;
-    unsigned long GTIN_UPC;
+    unsigned long long int GTIN_UPC;
     string Manufacturer;
     string Date_Modified;
     string Date_Available_Date;
@@ -28,7 +29,14 @@ void sort(vector<itemType>& myList);
 int numItems = 0;
 
 int main(){
-
+    ifstream inFile;
+    inFile.open("ProductsC.csv");
+    inFile.ignore(2e+13, '\n');
+    vector<itemType> items;
+    readFile(inFile, items);
+    for(itemType i : items){
+        printRecord(i);
+    }
 }
 
 void readFile( ifstream & inFile, vector<itemType> &item){
@@ -37,37 +45,48 @@ void readFile( ifstream & inFile, vector<itemType> &item){
            current;
     while(!inFile.eof()){ // While there is still data left in the file
         getline(inFile, line, '\n'); // Get the next row of data
+        cout << line;
         int i = 0, 
             data = 0;//The current datatype that the 
         while(getQuotedString(line, i, current)){ //While the current line of data still has data in it
+            cout << current;
             switch(data){
                 case 0:
                     temp.NBD_No = stoi(current);
+                    data++;
                     break;
                 case 1:
                     temp.Long_Name = current;
+                    data++;
                     break;
                 case 2:
                     temp.Data_Source = current;
+                    data++;
                     break;
                 case 3:
-                    temp.GTIN_UPC = stol(current);
+                    temp.GTIN_UPC = stoll(current);
+                    data++;
                     break;
                 case 4:
                     temp.Manufacturer = current;
+                    data++;
                     break;
                 case 5:
                     temp.Date_Modified = current;
+                    data++;
                     break;
                 case 6:
                     temp.Date_Available_Date = current;
+                    data++;
                     break;
                 case 7:
                     temp.Ingredients = current;
+                    data++;
                     break;
             }
         }
     temp.itemNumber = numItems;
+    cout << numItems;
     numItems++;
     item.push_back(temp);
     temp = itemType{};
@@ -75,23 +94,52 @@ void readFile( ifstream & inFile, vector<itemType> &item){
 
 }
 bool openInputFile( ifstream & inFile ){
-
+    return false;
 }
 bool getQuotedString( string& line, int &index, string & subString){
-
+    size_t pos, posEnd;
+    if(index == 0){
+        posEnd = line.find(',', index);
+        //cout << posEnd << line[posEnd];
+        if(line[index] == '"'){
+            subString = line.substr(index + 1, (posEnd - index - 1));
+        }else{
+            subString = line.substr(index, (posEnd - index));
+        }
+        index = posEnd+1;
+        return true;
+    }
+    pos = index;
+    //cout << pos << line[pos];
+    if(line[pos+1] == '"'){
+        index++;
+        posEnd = line.find('"',pos+1);
+    }else{
+        posEnd = line.find(',', pos);
+    }
+    //cout << posEnd << line[posEnd];
+    if(pos != string::npos){
+        subString = line.substr(index, (posEnd - index ));
+        index = posEnd+1;
+    }
+    if(index != line.length()){
+        return true;
+    }else{
+        return false;
+    }
 }
 bool findIngredient( itemType item, string ingredient){
-
+ return false;
 }
 string makeStringUpper( string s){
-
+    return s;
 }
 int searchForIngredient( vector<itemType> & itemType, string ingredient, bool printEnable ){
-
+    return 0;
 }
 void printRecord( itemType & item ){
-    cout << "Item # " << item.NDB_No<< endl;
-    cout << setprecision(14) << fixed();
+    cout << "Item # " << item.NBD_No<< endl;
+    cout << setprecision(14) << fixed;
     cout << "Name:" << item.Long_Name << endl;
     cout << "Manufacturer:" << item.Manufacturer<< endl;
     cout << "UPC:" << item.GTIN_UPC<< endl;
